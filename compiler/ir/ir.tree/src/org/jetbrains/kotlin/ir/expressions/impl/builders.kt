93daa6c5bd0926055dfb85a930a955639de3962a
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-05-01 12:41:40 +0200

    [IR] Remove type parameter from `IrConstKind`

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/impl/builders.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/impl/builders.kt
@@ -186,7 +186,7 @@ fun <T> IrConstImpl(
     startOffset: Int,
     endOffset: Int,
     type: IrType,
-    kind: IrConstKind<T>,
+    kind: IrConstKind,
     value: T,
 ) = IrConstImpl(
     constructorIndicator = null,
