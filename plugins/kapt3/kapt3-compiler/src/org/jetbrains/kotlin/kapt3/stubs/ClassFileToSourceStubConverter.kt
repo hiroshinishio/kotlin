84384ca4c885716ff9a2b5d48268b2cec98e4f05
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2024-06-06 19:02:14 +0200

    KAPT: Support varargs in annotations in correctErrorTypes mode
     #KT-69860

--- plugins/kapt3/kapt3-compiler/src/org/jetbrains/kotlin/kapt3/stubs/ClassFileToSourceStubConverter.kt
+++ plugins/kapt3/kapt3-compiler/src/org/jetbrains/kotlin/kapt3/stubs/ClassFileToSourceStubConverter.kt
@@ -38,10 +38,7 @@ import org.jetbrains.kotlin.fir.backend.FirAnnotationSourceElement
 import org.jetbrains.kotlin.fir.backend.FirMetadataSource
 import org.jetbrains.kotlin.fir.declarations.FirFile
 import org.jetbrains.kotlin.fir.declarations.FirResolvedImport
-import org.jetbrains.kotlin.fir.expressions.FirArrayLiteral
-import org.jetbrains.kotlin.fir.expressions.FirExpression
-import org.jetbrains.kotlin.fir.expressions.FirGetClassCall
-import org.jetbrains.kotlin.fir.expressions.arguments
+import org.jetbrains.kotlin.fir.expressions.*
 import org.jetbrains.kotlin.fir.resolve.diagnostics.ConeUnresolvedError
 import org.jetbrains.kotlin.fir.resolve.providers.symbolProvider
 import org.jetbrains.kotlin.fir.resolve.transformers.PackageResolutionResult
@@ -1428,10 +1425,19 @@ class ClassFileToSourceStubConverter(val kaptContext: KaptContextForStubGenerati
         name: String,
     ): JCExpression? {
         if (!isValidIdentifier(name)) return null
-        if (value !is FirArrayLiteral) return null
-
-        val expr = convertConstantValueArgumentsFir(containingClass, constantValue, value.arguments) ?: return null
-        return treeMaker.Assign(treeMaker.SimpleName(name), expr)
+        return when (value) {
+            is FirArrayLiteral -> {
+                val expr =
+                    convertConstantValueArgumentsFir(containingClass, constantValue, value.arguments) ?: return null
+                treeMaker.Assign(treeMaker.SimpleName(name), expr)
+            }
+            is FirVarargArgumentsExpression -> {
+                val expr =
+                    convertConstantValueArgumentsFir(containingClass, constantValue, value.arguments) ?: return null
+                treeMaker.Assign(treeMaker.SimpleName(name), expr)
+            }
+            else -> null
+        }
     }
 
     private fun convertConstantValueArgumentsFir(
