0997c4afe77b6e9e7125c0ce84e4890512e28b3f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-07-16 15:06:51 +0200

    [Kotlin for IDE build] deprecate K1 resolve packages
    We build kotlin-compiler-fe10-for-ide.jar, which contains Kotlin K1 compiler internals.
    Declarations from this jar are used by 3rd party plugins in IntelliJ.
    The K1 API is deprecated in favor of the Analysis API.
    
    This commit marks the K1 API as deprecated by adding the package-info.java deprecation file to them.
    This is done by generating a bunch of such files for each package we want to deprecate and putting them into the kotlin-compiler-fe10-for-ide.jar, which is a fat jar.
    
    ^KT-65469 fixed

---
+++ analysis/analysis-tools/deprecated-k1-frontend-internals-for-ide-generated/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    id("jps-compatible")
+}
+
+dependencies {
+    implementation(intellijRuntimeAnnotations())
+}
+
+project.configureJvmToolchain(JdkMajorVersion.JDK_17_0)
+
+tasks.withType<JavaCompile> {
+    // deprecating a package is a `@Deprecated annotation has no effect on packages` warning in Java,
+    // but we need to deprecate it for a 3rd party plugin compatibility checker
+    options.compilerArgs.remove("-Werror")
+}
+
+
+sourceSets {
+    "main" { generatedDir() }
+    "test" { none() }
+}
