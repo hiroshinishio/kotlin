87551465a0d9417af127099e99064ca631efa39d
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-07-24 04:20:27 +0200

    Update testData: annotation property order dependent change related to KT-54792

--- compiler/testData/codegen/box/annotations/instances/multimoduleCreation.kt
+++ compiler/testData/codegen/box/annotations/instances/multimoduleCreation.kt
@@ -54,12 +54,18 @@ class C {
 fun box(): String {
     val a = C().one()
     assertEquals(Int::class, a.kClass)
-    assertEquals(
-        """@kotlin.Metadata(bytecodeVersion=[1, 0, 3], data1=[], data2=[], extraInt=0, extraString=, kind=1, metadataVersion=[], packageName=)""",
+    assertContains(
+        listOf(
+            """@kotlin.Metadata(kind=1, metadataVersion=[], bytecodeVersion=[1, 0, 3], data1=[], data2=[], extraString=, packageName=, extraInt=0)""", // K2
+            """@kotlin.Metadata(bytecodeVersion=[1, 0, 3], data1=[], data2=[], extraInt=0, extraString=, kind=1, metadataVersion=[], packageName=)""", // K1
+        ),
         C().two().toString()
     )
-    assertEquals(
-        """@kotlin.Deprecated(level=WARNING, message=foo, replaceWith=@kotlin.ReplaceWith(expression=, imports=[]))""",
+    assertContains(
+        listOf(
+            """@kotlin.Deprecated(message=foo, replaceWith=@kotlin.ReplaceWith(expression=, imports=[]), level=WARNING)""", // K2
+            """@kotlin.Deprecated(level=WARNING, message=foo, replaceWith=@kotlin.ReplaceWith(expression=, imports=[]))""", // K1
+        ),
         C().three().toString()
     )
     val otherArraysStr = C().four().toString()
