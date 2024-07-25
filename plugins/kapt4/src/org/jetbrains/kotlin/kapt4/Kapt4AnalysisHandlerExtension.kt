aea0c1abae93005ddeb01ea278aa835f95d810e9
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2024-07-25 15:44:24 +0200

    Run K2 Kapt with IC support
     #KT-69860

--- plugins/kapt4/src/org/jetbrains/kotlin/kapt4/Kapt4AnalysisHandlerExtension.kt
+++ plugins/kapt4/src/org/jetbrains/kotlin/kapt4/Kapt4AnalysisHandlerExtension.kt
@@ -97,6 +97,7 @@ private class Kapt4AnalysisHandlerExtension : FirAnalysisHandlerExtension() {
                     }
             }
             put(JVMConfigurationKeys.SKIP_BODIES, true)
+            put(JVMConfigurationKeys.RETAIN_OUTPUT_IN_MEMORY, getBoolean(CommonConfigurationKeys.REPORT_OUTPUT_FILES))
         }
 
         val groupedSources: GroupedKtSources = collectSources(updatedConfiguration, project, messageCollector)
