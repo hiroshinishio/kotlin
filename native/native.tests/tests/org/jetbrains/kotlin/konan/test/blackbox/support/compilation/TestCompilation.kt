92fa8a6ca279be8446d7e355ed2f2f5134bf9ffd
Timofey Solonin <timofey.solonin@jetbrains.com>
2024-07-29 17:19:51 +0200

    Don't use -Xllvm-variant=dev
    ^KT-69094

--- native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/compilation/TestCompilation.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/compilation/TestCompilation.kt
@@ -79,7 +79,7 @@ abstract class BasicCompilation<A : TestCompilationArtifact>(
 
         // We use dev distribution for tests as it provides a full set of testing utilities,
         // which might not be available in user distribution.
-        add("-Xllvm-variant=dev")
+//        add("-Xllvm-variant=dev")
         addFlattened(binaryOptions.entries) { (name, value) -> listOf("-Xbinary=$name=$value") }
     }
 
