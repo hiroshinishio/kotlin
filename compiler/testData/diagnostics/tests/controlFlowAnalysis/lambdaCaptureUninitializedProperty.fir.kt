e5128dc695ec87685a04b1452bb93fcde509f106
Brian Norman <brian.norman@jetbrains.com>
2024-07-30 09:48:30 -0500

    fixup! [CFG] Introduce a new node and edge for variables captured by value

--- compiler/testData/diagnostics/tests/controlFlowAnalysis/lambdaCaptureUninitializedProperty.fir.kt
+++ compiler/testData/diagnostics/tests/controlFlowAnalysis/lambdaCaptureUninitializedProperty.fir.kt
@@ -1,11 +1,40 @@
 // DIAGNOSTICS: -UNUSED_PARAMETER
+// OPT_IN: kotlin.contracts.ExperimentalContracts
 // ISSUE: KT-70133
+
+import kotlin.contracts.*
+
+inline fun invokeInline(x: () -> Int, y: String) {
+    contract { callsInPlace(x, InvocationKind.UNKNOWN) }
+    x()
+}
+
+fun invokeInPlace(x: () -> Int, y: String) {
+    contract { callsInPlace(x, InvocationKind.UNKNOWN) }
+    x()
+}
 
 fun invokeLater(x: () -> Int, y: String) {
     x()
 }
 
-fun immutableInitAfterCapture() {
+fun immutableInitAfterCaptureInline() {
+    val x: String
+    invokeInline(
+        x = { x.length },
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+    )
+}
+
+fun immutableInitAfterCaptureInPlace() {
+    val x: String
+    invokeInPlace(
+        x = { <!UNINITIALIZED_VARIABLE!>x<!>.length },
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+    )
+}
+
+fun immutableInitAfterCaptureLater() {
     val x: String
     invokeLater(
         x = { <!UNINITIALIZED_VARIABLE!>x<!>.length },
@@ -13,7 +42,23 @@ fun immutableInitAfterCapture() {
     )
 }
 
-fun immutableInitBeforeCapture() {
+fun immutableInitBeforeCaptureInline() {
+    val x: String
+    invokeInline(
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+        x = { x.length },
+    )
+}
+
+fun immutableInitBeforeCaptureInPlace() {
+    val x: String
+    invokeInPlace(
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+        x = { x.length },
+    )
+}
+
+fun immutableInitBeforeCaptureLater() {
     val x: String
     invokeLater(
         y = if (true) { x = ""; "" } else { x = ""; "" },
@@ -21,7 +66,23 @@ fun immutableInitBeforeCapture() {
     )
 }
 
-fun mutableInitAfterCapture() {
+fun mutableInitAfterCaptureInline() {
+    var x: String
+    invokeInline(
+        x = { x.length },
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+    )
+}
+
+fun mutableInitAfterCaptureInPlace() {
+    var x: String
+    invokeInPlace(
+        x = { x.length },
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+    )
+}
+
+fun mutableInitAfterCaptureLater() {
     var x: String
     invokeLater(
         x = { x.length },
@@ -29,7 +90,23 @@ fun mutableInitAfterCapture() {
     )
 }
 
-fun mutableInitBeforeCapture() {
+fun mutableInitBeforeCaptureInline() {
+    var x: String
+    invokeInline(
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+        x = { x.length },
+    )
+}
+
+fun mutableInitBeforeCaptureInPlace() {
+    var x: String
+    invokeInPlace(
+        y = if (true) { x = ""; "" } else { x = ""; "" },
+        x = { x.length },
+    )
+}
+
+fun mutableInitBeforeCaptureLater() {
     var x: String
     invokeLater(
         y = if (true) { x = ""; "" } else { x = ""; "" },
