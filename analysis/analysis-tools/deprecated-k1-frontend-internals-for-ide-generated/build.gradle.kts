faa07ead3c7d2f885d6101dcd3fc9b3d4c78f8a0
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-07-30 11:54:22 +0200

    fixup! [Kotlin for IDE build] deprecate K1 resolve packages

--- analysis/analysis-tools/deprecated-k1-frontend-internals-for-ide-generated/build.gradle.kts
+++ analysis/analysis-tools/deprecated-k1-frontend-internals-for-ide-generated/build.gradle.kts
@@ -13,6 +13,8 @@ tasks.withType<JavaCompile> {
     // deprecating a package is a `@Deprecated annotation has no effect on packages` warning in Java,
     // but we need to deprecate it for a 3rd party plugin compatibility checker
     options.compilerArgs.remove("-Werror")
+    targetCompatibility = "1.8"
+    sourceCompatibility = "1.8"
 }
 
 
